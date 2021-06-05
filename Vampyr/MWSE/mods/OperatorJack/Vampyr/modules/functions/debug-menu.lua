aa0b956ea8a0214d937ea1f0c17a178abbad5efa
OperatorJack <thepaladinof@gmail.com>
2021-06-04 22:07:53 -0400

    Add more advanced debug menu tools for modifying base and current blood.

--- Vampyr/MWSE/mods/OperatorJack/Vampyr/modules/functions/debug-menu.lua
+++ Vampyr/MWSE/mods/OperatorJack/Vampyr/modules/functions/debug-menu.lua
@@ -1,18 +1,38 @@
 local common = require("OperatorJack.Vampyr.common")
 local blood = require("OperatorJack.Vampyr.modules.blood")
 
-local function keyDownEqual(eventKeyDown, configKeyDown)
-    if eventKeyDown.keyCode == configKeyDown.keyCode and
-        eventKeyDown.isAltDown == configKeyDown.isAltDown and
-        eventKeyDown.isShiftDown == configKeyDown.isShiftDown and
-        eventKeyDown.isControlDown == configKeyDown.isControlDown then
-           return true
-    end
-    return false
+local bloodMenuVals = {
+    [1] = -500,
+    [2] = -50,
+    [3] = -5,
+    [4] = 5,
+    [5] = 50,
+    [6] = 500,
+}
+
+local function showBloodMenu(func)
+        local message = "Vampyr Debug Menu - Blood Levels"
+        local buttons = {}
+        for _, bloodAmount in ipairs(bloodMenuVals) do
+            table.insert(buttons, {
+                text = tostring(bloodAmount),
+                callback = function()
+                    func(bloodAmount)
+                    local bloodStat = blood.getPlayerBloodStatistic()
+                    tes3.messageBox("Current: " .. bloodStat.current .. " , Max: " .. bloodStat.base)
+                end,
+            })
+        end
+
+        common.messageBox{
+            message = message,
+            buttons = buttons,
+            doesCancel = true
+        }
 end
 
 local function debugMenuKey(e)
-    if common.config.enableDebugMenu == true and keyDownEqual(e, common.config.debugMenuKey) == true then
+    if common.config.enableDebugMenu == true and common.keyDownEqual(e, common.config.debugMenuKey) == true then
         local message = "Vampyr Debug Menu"
         local buttons = {
             {
@@ -36,24 +56,21 @@ local function debugMenuKey(e)
                 },
             },
             {
-                text = "Add 50 Base Blood",
+                text = "Modify Base Blood",
                 requirements = common.isPlayerVampire,
                 callback = function()
-                    blood.modPlayerBaseBloodStatistic(50)
-                    local bloodStat = blood.getPlayerBloodStatistic()
-                    tes3.messageBox("Current: " .. bloodStat.current .. " , Max: " .. bloodStat.base)
+                    showBloodMenu(blood.modPlayerBaseBloodStatistic)
+
                 end,
                 tooltipDisabled = {
                     text = "You must be a vampire."
                 },
             },
             {
-                text = "Add 50 Current Blood",
+                text = "Modify Current Blood",
                 requirements = common.isPlayerVampire,
                 callback = function()
-                    blood.modPlayerCurrentBloodStatistic(50)
-                    local bloodStat = blood.getPlayerBloodStatistic()
-                    tes3.messageBox("Current: " .. bloodStat.current .. " , Max: " .. bloodStat.base)
+                    showBloodMenu(blood.modPlayerCurrentBloodStatistic)
                 end,
                 tooltipDisabled = {
                     text = "You must be a vampire."

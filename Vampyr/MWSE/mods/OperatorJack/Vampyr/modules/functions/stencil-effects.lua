bfcbe7c2d4ee94e2c4357677b2f38e3afd67abd8
OperatorJack <thepaladinof@gmail.com>
2021-06-12 21:12:45 -0400

    Use different stencils for player, npc, and creatures.

--- Vampyr/MWSE/mods/OperatorJack/Vampyr/modules/functions/stencil-effects.lua
+++ Vampyr/MWSE/mods/OperatorJack/Vampyr/modules/functions/stencil-effects.lua
@@ -2,10 +2,10 @@ local common = require("OperatorJack.Vampyr.common")
 
 local functions = {}
 
-local stencil = nil
-local stencilArmsAndLegs = nil
-
 local stenciledActors = {}
+
+local masks = {}
+
 local vanillaStencilProperties = {}
 local vanillaStencilObjects = {
     ["Left Upper Leg"] = true,
@@ -45,57 +45,83 @@ functions.detachStencilProperty = function(reference)
     common.logger.debug("Removed stencil properties from %s.", reference)
 end
 
-functions.attachStencilProperty = function(reference)
-    if stenciledActors[reference] then return end
+local function attachStencilPropertyToReference(reference, mask)
+    reference.sceneNode:detachProperty(0x3)
+    reference.sceneNode:attachProperty(mask)
+    reference.sceneNode:update()
+    reference.sceneNode:updateNodeEffects()
+    reference.sceneNode:updateProperties()
+    stenciledActors[reference] = true
+end
 
-    local sceneNode = reference.sceneNode
-    sceneNode:detachProperty(0x3)
-
-    if not stencil then
-        stencil = tes3.loadMesh(common.paths.stencil):getProperty(0x3)
-    end
-
-    -- Load override for arms & legs vanilla stencil property.
-    if not stencilArmsAndLegs then
-        stencilArmsAndLegs = tes3.loadMesh(common.paths.stencilArmsAndLegs):getProperty(0x3)
-    end
-
+local function attachStencilMirrorPropertiesToReference(reference, mask)
     -- Replace vanilla arm and leg stencil property. Cache to reset later.
     for name in pairs(vanillaStencilObjects) do
-        local node = sceneNode:getObjectByName(name)
+        local node = reference.sceneNode:getObjectByName(name)
         if node then
             vanillaStencilProperties[name] = node:getProperty(0x3)
             node:detachProperty(0x3)
-            node:attachProperty(stencilArmsAndLegs)
+            node:attachProperty(mask)
         end
     end
+end
 
-    sceneNode:attachProperty(stencil)
-    sceneNode:update()
-    sceneNode:updateNodeEffects()
-    sceneNode:updateProperties()
-    stenciledActors[reference] = true
+functions.attachStencilProperty = function(reference)
+    if stenciledActors[reference] then return end
+
+    local mask
+    local maskMirror
+
+    -- Set mask paths & process
+    if reference == tes3.player then
+        mask= masks.player
+        maskMirror = masks.playerMirror
+
+        attachStencilMirrorPropertiesToReference(tes3.player, maskMirror)
+        --attachStencilMirrorPropertiesToReference(tes3.firstPersonReference, maskMirror)
+
+        attachStencilPropertyToReference(tes3.player, mask)
+        --attachStencilPropertyToReference(tes3.firstPersonReference, mask)
+
+    elseif reference.object.objectType == tes3.objectType.npc then
+        mask= masks.npc
+        maskMirror = masks.npcMirror
+
+        attachStencilMirrorPropertiesToReference(reference, maskMirror)
+        attachStencilPropertyToReference(reference, mask)
+
+    elseif reference.object.objectType == tes3.objectType.creature then
+        mask = masks.creature
+
+        attachStencilPropertyToReference(reference, mask)
+
+    end
 
     common.logger.debug("Added stencil properties to %s.", reference)
 end
 
 
+-- Handle initializing and rebuilding scenegraph for stenciled actors.
 event.register("referenceSceneNodeCreated", function(e)
-    if e.reference.object.objectType == tes3.objectType.npc or
+    if  e.reference.object.objectType == tes3.objectType.npc or
         e.reference.object.objectType == tes3.objectType.creature or
         e.reference == tes3.player then
-        functions.attachStencilProperty(e.reference)
-    end
-end)
-
--- Handle rebuilding scenegraph for stenciled actors.
-event.register("referenceSceneNodeCreated", function(e)
-    if stenciledActors[e.reference] then
         stenciledActors[e.reference] = nil
         functions.attachStencilProperty(e.reference)
     end
 end)
+
 -- When invalidated, scene node will be recreated. Remove from tracking.
 event.register("objectInvalidated", function(e)
     stenciledActors[e.object] = nil
+end)
+
+event.register("initialized", function(e)
+    masks = {
+        player = tes3.loadMesh("OJ\\stencils\\mask_char.nif"):getProperty(0x3),
+        playerMirror = tes3.loadMesh("OJ\\stencils\\mask_char_mirror.nif"):getProperty(0x3),
+        npc = tes3.loadMesh("OJ\\stencils\\mask_npc.nif"):getProperty(0x3),
+        npcMirror = tes3.loadMesh("OJ\\stencils\\mask_npc_mirror.nif"):getProperty(0x3),
+        creature = tes3.loadMesh("OJ\\stencils\\mask_creature.nif"):getProperty(0x3)
+    }
 end)
